2a55742d0f14c5fbbd57487682a24d73b0f94f8c
Zxt34 <3265923652@qq.com>
2021-08-22 14:03:54 +0800

    项目单元测试

---
+++ ProblemDAOTest.java
@@ -0,0 +1,91 @@
+package problem;
+
+public class ProblemDAOTest {
+
+//    @Test
+//    public void insert() {
+//        Problem problem = new Problem();
+//        problem.setId(2);
+//        problem.setTitle("两数之和");
+//        problem.setLevel("简单");
+//        problem.setDescription("给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。\n" +
+//                "\n" +
+//                "你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。\n" +
+//                "\n" +
+//                "你可以按任意顺序返回答案。\n" +
+//                "\n" +
+//                " \n" +
+//                "\n" +
+//                "示例 1：\n" +
+//                "\n" +
+//                "输入：nums = [2,7,11,15], target = 9\n" +
+//                "输出：[0,1]\n" +
+//                "解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。\n" +
+//                "示例 2：\n" +
+//                "\n" +
+//                "输入：nums = [3,2,4], target = 6\n" +
+//                "输出：[1,2]\n" +
+//                "示例 3：\n" +
+//                "\n" +
+//                "输入：nums = [3,3], target = 6\n" +
+//                "输出：[0,1]\n" +
+//                " \n" +
+//                "\n" +
+//                "提示：\n" +
+//                "\n" +
+//                "2 <= nums.length <= 103\n" +
+//                "-109 <= nums[i] <= 109\n" +
+//                "-109 <= target <= 109\n" +
+//                "只会存在一个有效答案\n" +
+//                "\n" +
+//                "来源：力扣（LeetCode）\n" +
+//                "链接：https://leetcode-cn.com/problems/two-sum\n" +
+//                "著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。");
+//        problem.setTemplateCode("class Solution {\n" +
+//                "    public int[] twoSum(int[] nums, int target) {\n" +
+//                "\n" +
+//                "    }\n" +
+//                "}");
+//        problem.setTestCode("    public static void main(String[] args) {\n" +
+//                "        Solution solution = new Solution();\n" +
+//                "        int[] arr = {2,7,11,15};\n" +
+//                "        int target = 9;\n" +
+//                "        int[] result = solution.twoSum(arr, 9);\n" +
+//                "        if (result.length == 2 && result[0] == 1 && result[1] == 2) {\n" +
+//                "            System.out.println(\"TestCase OK!\");\n" +
+//                "        } else {\n" +
+//                "            System.out.println(\"TestCase Failed! arr: {2, 7, 11, 15}, target: 9\");\n" +
+//                "        }\n" +
+//                "\n" +
+//                "        int[] arr2 = {3,2,4};\n" +
+//                "        int target2 = 6;\n" +
+//                "        int[] result2 = solution.twoSum(arr2, target2);\n" +
+//                "        if (result2.length == 2 && result2[0] == 1 && result2[1] == 2) {\n" +
+//                "            System.out.println(\"TestCaseOK!\");\n" +
+//                "        } else {\n" +
+//                "            System.out.println(\"TestCaseFailed!\");\n" +
+//                "        }\n" +
+//                "    }\n");
+//        ProblemDAO problemDAO = new ProblemDAO();
+//        problemDAO.insert(problem);
+//    }
+
+//    @Test
+//    public void delete() {
+//        ProblemDAO problemDAO = new ProblemDAO();
+//        problemDAO.delete(2);
+//    }
+
+//    @Test
+//    public void selectAll() {
+//        ProblemDAO problemDAO = new ProblemDAO();
+//        List<Problem> problems = problemDAO.selectAll();
+//        System.out.println(problems);
+//    }
+
+//    @Test
+//    public void selectOne() {
+//        ProblemDAO problemDAO = new ProblemDAO();
+//        System.out.println(problemDAO.selectOne(1));
+//    }
+}
