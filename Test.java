e10c64a227296c4063d69bb4e224e9a20cd6fc53
Zxt34 <3265923652@qq.com>
2021-08-23 20:34:44 +0800

    面试

---
+++ Test.java
@@ -0,0 +1,21 @@
+public class Test {
+//    public int[] getFraction (float a) {
+//        int[] res = new int[2];
+//        for (int i = 1; i <= 200; i++) {
+//            for (int j = 3; j < 198; j++) {
+//                int num = j / i;
+//                if(-0.0000000005 < (a - num) && 0.0000000005 > (a - num)){
+//                    res[0] = j;
+//                    res[1] = i;
+//                }
+//            }
+//        }
+//        return res;
+
+//        ArrayList<ArrayList<String>> res = new ArrayList<>();
+//        for (int i = 0; i < strs.length; i++) {
+//            char cs = strs[i].toCharArray();
+//            Arrays.sort(cs);
+//        }
+//    }
+}
